d939e6ffeea6b287762d5d389680215e667658e0
Dominika-Kuros <113472378+Dominika-Kuros@users.noreply.github.com>
2023-10-29 18:45:44 +0100

    deploy

--- src/app.ts
+++ src/app.ts
@@ -16,10 +16,10 @@ const app = express();
 app.use(morgan("dev"));
 
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "../client/build")));
+app.use(express.static(path.join(__dirname, "./client/build")));
 app.get("*", function (_, res) {
   res.sendFile(
-    path.join(__dirname, "../client/build/index.html"),
+    path.join(__dirname, "./client/build/index.html"),
     function (err) {
       res.status(500).send(err);
     }
