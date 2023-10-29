81e1335301b9a6f3e0178b535b5f678829392be8
Dominika-Kuros <113472378+Dominika-Kuros@users.noreply.github.com>
2023-10-29 22:54:16 +0100

    deploy

--- backend/src/models/user.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var mongoose_1 = require("mongoose");
-var userSchema = new mongoose_1.Schema({
-    username: { type: String, required: true, unique: true },
-    email: { type: String, required: true, unique: true, select: false },
-    password: { type: String, required: true, select: false }
-});
-exports["default"] = (0, mongoose_1.model)("User", userSchema);
