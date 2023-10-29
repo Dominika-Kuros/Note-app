7106ec52e54bad9ba48211e804d7f0d096a968ba
Dominika-Kuros <113472378+Dominika-Kuros@users.noreply.github.com>
2023-10-29 22:22:42 +0100

    deploy

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,14 @@
 "use strict";
-exports.__esModule = true;
-var app_1 = require("./src/app");
-var validateEnv_1 = require("./src/util/validateEnv");
-var mongoose_1 = require("mongoose");
+export const __esModule = true;
+import app from "./src/app";
+import validateEnv_1 from "./src/util/validateEnv";
+import mongoose_1 from "mongoose";
 var port = validateEnv_1["default"].PORT;
 mongoose_1["default"]
   .connect(validateEnv_1["default"].MONGO_CONNECTION_STRING)
   .then(function () {
     console.log("Mongoose connected");
-    app_1["default"].listen(port, function () {
+    app["default"].listen(port, function () {
       console.log("Server running on port: " + port);
     });
   })
