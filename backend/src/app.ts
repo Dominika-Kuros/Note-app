525357c844f72c56646b959b80a0eb3a8caacd45
Dominika-Kuros <113472378+Dominika-Kuros@users.noreply.github.com>
2023-10-29 22:18:06 +0100

    deploy

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,27 +1,14 @@
-import MongoStore from "connect-mongo";
-import cors from "cors";
 import "dotenv/config";
 import express, { NextFunction, Request, Response } from "express";
-import session from "express-session";
-import createHttpError, { isHttpError } from "http-errors";
-import morgan from "morgan";
-import requiresAuth from "./middleware/auth";
 import notesRoutes from "./routes/notes";
 import userRoutes from "./routes/users";
+import morgan from "morgan";
+import createHttpError, { isHttpError } from "http-errors";
+import session from "express-session";
 import env from "./util/validateEnv";
-import mongoose from "mongoose";
-
-const port = env.PORT;
-
-mongoose
-  .connect(env.MONGO_CONNECTION_STRING)
-  .then(() => {
-    console.log("Mongoose connected");
-    app.listen(port, () => {
-      console.log("Server running on port: " + port);
-    });
-  })
-  .catch(console.error);
+import MongoStore from "connect-mongo";
+import requiresAuth from "./middleware/auth";
+import cors from "cors";
 
 const app = express();
 
@@ -29,7 +16,7 @@ app.use(morgan("dev"));
 
 app.use(express.json());
 
-app.use(cors({ origin: "*" }));
+app.use(cors());
 
 app.use(
   session({
