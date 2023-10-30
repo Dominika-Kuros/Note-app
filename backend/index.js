c9f6ac1566407e6e8a0f3d21aa2c15713e2cdbf7
Dominika-Kuros <113472378+Dominika-Kuros@users.noreply.github.com>
2023-10-30 07:35:19 +0100

    deploy

--- backend/index.js
+++ backend/index.js
@@ -1,22 +1,14 @@
-import { join } from "node:path";
-("use strict");
-var __importDefault =
-  (this && this.__importDefault) ||
-  function (mod) {
-    return mod && mod.__esModule ? mod : { default: mod };
-  };
-Object.defineProperty(exports, "__esModule", { value: true });
-const app_1 = __importDefault.require(join(process.cwd() + ".dist/src/app"));
-const validateEnv_1 = __importDefault.require(
-  join(process.cwd()(".dist/src/util/validateEnv"))
-);
-const mongoose_1 = __importDefault.require(join(process.cwd()("mongoose")));
-const port = validateEnv_1.default.PORT;
-mongoose_1.default
-  .connect(validateEnv_1.default.MONGO_CONNECTION_STRING)
+import app from "./src/app";
+import env from "./src/util/validateEnv";
+import mongoose from "mongoose";
+
+const port = env.PORT;
+
+mongoose
+  .connect(env.MONGO_CONNECTION_STRING)
   .then(() => {
     console.log("Mongoose connected");
-    app_1.default.listen(port, () => {
+    app.listen(port, () => {
       console.log("Server running on port: " + port);
     });
   })
